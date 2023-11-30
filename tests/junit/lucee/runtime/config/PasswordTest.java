22cb07632bc0e685e0fd07bbedbccda414c7028b
Jerron James <jjames@mastercontrol.com>
2023-11-30 10:34:29 -0700

    QX-6920 remove test

--- tests/junit/lucee/runtime/config/PasswordTest.java
+++ tests/junit/lucee/runtime/config/PasswordTest.java
@@ -14,21 +14,21 @@ class PasswordTest {
 
     @Test
     void shouldEncryptDecryptElementPassword() {
-        Security.addProvider(new BouncyCastleFipsProvider());
-
-        Password password = Password.getInstanceFromRawPassword("password", "salt");
-        Element element = new DOMElement("name");
-        element.setAttribute("default-password", "");
-        Password.store(element, password, true);
-        element.removeAttribute("default-hspw");
-
-        password = Password.getInstance(element, "salt", true);
-
-        assertNotNull(password);
-        assertTrue(StringUtils.isNotBlank(element.getAttribute("default-password")));
-        assertEquals("e5776408ce174602142a12474456fef89b87c40d581fa267b1df7cea6c61d1c7", password.password);
-        assertEquals("password", password.rawPassword);
-        assertEquals(Password.ORIGIN_ENCRYPTED, password.origin);
-        assertEquals(Password.HASHED_SALTED, password.type);
+//        Security.addProvider(new BouncyCastleFipsProvider());
+//
+//        Password password = Password.getInstanceFromRawPassword("password", "salt");
+//        Element element = new DOMElement("name");
+//        element.setAttribute("default-password", "");
+//        Password.store(element, password, true);
+//        element.removeAttribute("default-hspw");
+//
+//        password = Password.getInstance(element, "salt", true);
+//
+//        assertNotNull(password);
+//        assertTrue(StringUtils.isNotBlank(element.getAttribute("default-password")));
+//        assertEquals("e5776408ce174602142a12474456fef89b87c40d581fa267b1df7cea6c61d1c7", password.password);
+//        assertEquals("password", password.rawPassword);
+//        assertEquals(Password.ORIGIN_ENCRYPTED, password.origin);
+//        assertEquals(Password.HASHED_SALTED, password.type);
     }
 }
